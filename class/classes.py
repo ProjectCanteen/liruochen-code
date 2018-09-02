a862f579aef92df552bfacbe8611b1a36ec3f31d
若琛 李 <liruochen2008@live.cn>
2018-09-02 19:36:53 +0800

    将部分B导入C

--- class/classes.py
+++ class/classes.py
@@ -1,83 +1,85 @@
 #类模块（class.py）      对学生，员工和管理员的数据建模供主模块调用
 #本模块需调用B模块
 
+import file_operate     #导入B模块
 #定义学生类
 class student():
-    def __init__(self,user_name,key,name):
+    def __init__(self,user_name,key,name,identity):
         '''
         定义学生类属性
         user_name用户名
         key密码
         name姓名
+        identity身份
         '''
         self.user_name=user_name
         self.key=key
         self.user_name=name
+        self.identity='student'
 
-    def get_today_menu(self,date):
+    def get_today_menu(self,date,window):
         #定义获取今日菜单的方法
         #需要调用B模块
-        #参数：date（日期）
-        pass
+        #参数：date（日期）,window(窗口号）
+        return(file_operate.get_menu(date,window))      #返回菜单列表
         
-    def creat_order(self,order_list):
+    def creat_order(self,order_list,date,window):
         #定义创建订单的方法
-        #参数：order_list（含订单的列表）
-        #返回一个字典，key为学生类，value为订单列表
-        order_dict=dict()
-        order_dict[self]=order_list
-        return(order_dict)
+        #参数：order_list（含订单的列表），date（日期），window（窗口号）
+        #将数据结构保存
+        file_operate.creat_stumenu(self,order_list,date,window)
 
-    def change_order(self,delete_item,add_item,old_order_list):
+    def change_order(self,delete_item,add_item,stu,window,date):
         #定义更改订单的方法
-        #参数：delete_item（删除项）,add_item（新增项）,old_order_list（原订单列表）
-        #返回一个字典，key为日期，value为新订单列表
+        #参数：delete_item（删除项）,add_item（新增项）,old_order_list（原订单列表），stu（学生对象），window（窗口号），date（日期）
+        #返回一个字典，key为日期，value为新订单列表,并将新的数据结构保存
+        old_order_dict=file_operate.delete_stumenu(date,stu.user_name,window)       #旧订单已删除
+        old_order_list=old_order_dict.values()[0]
         new_order_list=old_order_list.copy()
         new_order_list.remove(delete_item)
-        new_order_list.append(add_item)
-        new_order_dict=dict()
-        new_order_dict[self]=new_order_dict
-        return(new_order_dict)
+        new_order_list.append(add_item)     #进行数据的删改
+        new_order_dict=file_operate.creat_stumenu(stu,new_order_list,date,window)       #创建新订单
+        return(new_order_dict)      #返回新订单
 
-    def delete_order(self,date):
+    def delete_order(self,date,stu,window):
         #定义删除订单的方法
-        #参数：date日期
+        #参数：date日期，stu学生对象，window窗口号
+        #返回已删除的数据
         #需调用B模块
-        pass
-
-
-
-        
-        
-        
+        old_order_dict=file_operate.delete_stumenu(date,stu.user_name,window)       #删除数据
+        return(old_order_dict)  #返回已删除的数据
+                        
 #定义员工类
 class stuff():
-    def __init__(self,user_name,key,name):
+    def __init__(self,user_name,key,name,identity):
         self.user_name=user_name
         self.key=key
         self.user_name=name
+        self.identity='stuff'
         '''
         定义员工类的属性
         user_name用户名
         key密码
         name姓名
+        identity身份
         '''
 
-    def get_menu(self,date):
+    def get_menu(self,date,window):
         #查看菜单
         #需调用B模块
         #返回含菜单的列表
-        #参数：date（日期）
-        pass
+        #参数：date（日期），window（窗口号）
+        return(file_operate.get_menu(date,window))      #返回菜单列表
         
-    def creat_menu(self,date,menu_list):
-        menu_dict=dict()
-        menu_dict[date]=menu_list
-        return(menu_dict)
+        
+    def creat_menu(self,date,menu_list,window):
+        menu=file_operate.creat_menu(menu_list,date,window)
+        return(menu)        #返回菜单
         #创建菜单
-        #参数：date（日期），menu_list（含菜单的列表）
-        #返回一个字典，key为date，value菜单列表
+        #参数：date（日期），menu_list（含菜单的列表）,window(窗口号）
+        #返回一个字典，key为date，value菜单列表，保存新的数据结构
 
+#更改至此
     def change_menu(self,date,old_list,delete_item,add_item):
         new_list=old_list.copy()
         new_list.remove(delete_item)
@@ -98,15 +100,17 @@ class stuff():
 
 #定义管理员类
 class admin():
-    def __init__(self,user_name,key,name):
+    def __init__(self,user_name,key,name,identity):
         self.user_name=user_name
         self.key=key
         self.name=name
+        self.identity=identity
         '''
         管理员类属性初始化
         user_name用户名
         key密码
         name姓名
+        identity身份
         '''
 
     def add_stuaccount(self,user_name,key,name):
--- class/file_operate.py
+++ class/file_operate.py
@@ -2,55 +2,24 @@
 import pickle       #导入pickle模块
 import os       #导入os模块
 
-#函数login，参数：用户名，密码，用户身份，访问存放用户登录信息的文件，返回布尔值。
+#函数login，参数：用户名，密码，用户身份，用户身份必须为'student','stuff','admin'中的一种，访问存放用户登录信息的文件，返回布尔值。
 
 def login(user_name,key,user_identity):
-    if user_identity=='student':
+    def check(user_name,key,user_identity):
         try:
-            with open('../data/user_info/student.pickle','rb') as f:
-                stu_file=pickle.load(f)     #stu_file是存放学生类的列表
-        except IOError as err:
-            print(str(err))
-        for each_stu in stu_file:
-            if each_stu.user_name==user_name and each_stu.key==key:
-                i=True      #判断是否找到对应用户
-                break
-            if i==True:
-                return(True)
-            else:
-                return(False)    
-    elif user_identity=='stuff':
-        try:
-            with open('../data/user_info/stuff.pickle','rb') as f:
-                stuff_file=pickle.load(f)
-        except IOError as err:
-            print(str(err))
-        for each_stuff in stuff_file:
-            if each_stuff.user_name==user_name and each_stuff.key==key:
-                i=True
-                break
-            if i==True:
+            with open('../data/user_info/'+str(user_identity)+'/'+str(user_name)+'.pickle','rb') as f:
+                user=pickle.load(f)
+            if user.key==key:
                 return(True)
             else:
                 return(False)
-    elif user_identity=='admin':
-        try:
-            with open('../data/user_info/admin.pickle','rb') as f:
-                admin_file=pickle.load(f)
-        except IOError as err:
-            print(str(err))
-        for each_admin in admin_file:
-            if each_admin.user_name==user_name and each_admin.key==key:
-                i=True
-                break
-            if i==True:
-                return(True)
-            else:
-                return(False)
-    else:
-        print('User_identity is illegal!')
+        except IOError:
+            return('用户不存在！')        #若文件不存在，则返回错误信息
 
-#函数creat_stumenu，参数：学生订单和日期和窗口号，一个学生类，创建字典，key为学生类，value为订单列表，订单列表第1项是窗口号，用pickle保存在本地
+    response=check(user_name,key,user_identity)
+    return(response)        #最终的返回值
+   
+#函数creat_stumenu，参数：学生订单和日期和窗口号，一个学生类，创建字典，key为学生类，value为订单列表，订单列表第1项是窗口号，用pickle保存在本地，返回新的订单
 
 def creat_stumenu(stu,order_list,date,window):
     #项目的日期格式均为"18-8-31"的格式，便于字符串操作
@@ -59,9 +28,9 @@ def creat_stumenu(stu,order_list,date,window):
             today_stumenu=pickle.load(f)
             #today_stumenu是列表，各元素是字典，key为学生类，value为订单字典
     except IOError as err:
-        print(str(err))
+        return(str(err))        #若出错，则将错误信息返回
     order_dict=dict()
-    order_list.append(window)
+    order_list.append(window)       #列表第一项是窗口号
     order_dict[stu]=order_list
     if isinstance(today_stumenu,list):
         today_stumenu.append(order_dict)        #新增数据
@@ -73,7 +42,7 @@ def creat_stumenu(stu,order_list,date,window):
             pickle.dump(today_stumenu,f)
             #保存更改后的文件
     except IOError as err:
-        print(str(err))
+        return(str(err))
     try:
         with open('../data/order_now_info/'+str(window)+'/'+str(date)+'_ordernow.pickle','rb') as f:
             order_now_dict=pickle.load(f)       #字典
@@ -82,20 +51,20 @@ def creat_stumenu(stu,order_list,date,window):
         with open('../data/order_now_info/'+str(window)+'/'+str(date)+'_ordernow.pickle','wb') as f:
             pickle.dump(order_now_dict,f)       #更新订单状态
     except IOError as err:
-        print(str(err))
+        return(str(err))
+    return(order_dict)
 
-
-#函数create_menu，参数：员工创建的菜单列表和日期和窗口号，创建字典，key为日期，value为菜单列表，菜单列表第一项是窗口号，用pickle保存在本地
+#函数create_menu，参数：员工创建的菜单列表和日期和窗口号，创建字典，key为日期，value为菜单列表，菜单列表第一项是窗口号，用pickle保存在本地，并返回菜单
 
 def creat_menu(menu_list,date,window):
-    menu_list.append(window)
+    menu_list.append(window)        #列表第一项是窗口号
     menu_dict=dict()
     menu_dict[date]=menu_list
     try:
         with open('../data/menu_info/'+str(window)+'/'+str(date)+'_menu.pickle','wb') as f:
             pickle.dump(menu_dict,f)
     except IOError as err:
-        print(str(err))
+        return(str(err))        #若出错，则将错误信息返回
     order_now_dict=dict()       #新增订单状态
     for item in menu_list:
         order_now_dict[item]=0
@@ -103,7 +72,8 @@ def creat_menu(menu_list,date,window):
         with open('../data/order_now_info/'+str(window)+'/'+str(date)+'_ordernow.pickle','wb') as f:
             pickle.dump(order_now_dict,f)       #创建订单状态字典
     except IOError as err:
-        print(str(err))
+        return(str(err))
+    return(menu_dict)   #返回菜单
 
 
 #函数get_menu，参数：日期，窗口号，返回存放菜单的列表
@@ -113,24 +83,23 @@ def get_menu(date,window):
         with open('../data/menu_info/'+str(window)+'/'+str(date)+'_menu.pickle','rb') as f:
             menu_file=pickle.load(f)    #menu_list是菜单列表
     except IOError as err:
-        print(str(err))
-        return(None)
+        return(str(err))        #若出错，则将错误信息返回
     return(menu_file[1:])       #列表第一项是窗口号，因此需进行切片操作
 
-#函数delete_stumenu,参数：日期，用户名，窗口号，删除该学生该日的订单,若该学生该日该窗口无订单，则返回False
+#函数delete_stumenu,参数：日期，用户名，窗口号，删除并返回该学生该日的订单,若该学生该日该窗口无订单，则返回False
 
 def delete_stumenu(date,user_name,window):
     try:
         with open('../data/order_info/'+str(window)+'/'+str(date)+'_order.pickle','rb') as f:
             order_file=pickle.load(f)       #order_file是一个列表，元素是字典，详细内容参上
     except IOError as err:
-        print(str(err))
+        return(str(err))        #若出错，则将错误信息返回
     for each_order in order_file:
         key_list=list(each_order.keys())
         key=key_list[0]
         if user_name==key.user_name:
             deleted_menu=each_order
-            order_file.remove(each_order)
+            order_file.remove(deleted_menu)
             i=True
             break
     if i!=True:
@@ -143,6 +112,7 @@ def delete_stumenu(date,user_name,window):
         order_now_dict[item]=order_now_dict[item]-1
     with open('../data/order_now_info/'+str(window)+'/'+str(date)+'_ordernow.pickle','wb') as f:
         pickle.dump(order_now_dict,f)       #更新订单状态
+    return(deleted_menu)        #将旧订单返回
 
 #函数delete_menu,参数：日期，窗口号，删除该日该窗口的菜单
 
@@ -153,42 +123,16 @@ def delete_menu(date,window):
         file_1='../data/order_now_info/'+str(window)+'/'+str(date)+'_ordernow.pickle'
         os.remove(file_1)
     except:
-        print('菜单未创建！')
+        return('菜单未创建！')        #若出错，则将错误信息返回
 
-#函数creat_newstu,参数：一个学生类，打开存放学生类的pickle，向列表中新增元素
+#函数creat_newuser,参数：类，向专用文件夹中添加文件
 
-def creat_newstu(stu):
-    try:
-        with open('../data/user_info/student.pickle','rb') as f:
-            stu_file=pickle.load(f)     #stu_file是存放学生类的列表
-        if isinstance(stu_file,list):
-            stu_file.append(stu)
-        else:
-            stu_file=[]
-            stu_file.append(stu)
-        with open('../data/user_info/student.pickle','wb') as f:
-            pickle.dump(stu_file,f)
-    except IOError as err:
-        print(str(err))
+def creat_newuser(class_):
+    def creat_newfile(class_,name,identity):
+        with open('../data/user_info/'+str(identity)+'/'+str(name)+'/.pickle','wb') as f:
+            pickle.dump(class_,f)
 
-#函数creat_newstuff,参数：一个员工类，打开存放员工类的pickle，向列表中新增元素
-
-def creat_newstuff(stuff):
-    try:
-        with open('../data/user_info/stuff.pickle','rb') as f:
-            stuff_file=pickle.load(f)
-    except IOError as err:
-        print(str(err))
-    if isinstance(stuff_file,list):
-        stuff_file.append(stuff)
-    else:
-        stuff_file=[]
-        stuff_file.append(stuff)
-    try:
-        with open('../data/user_info/stuff.pickle','wb') as f:
-            pickle.dump(stuff_file,f)
-    except IOError as err:
-        print(str(err))
+    creat_newfile(class_,class_.user_name,class_.identity)
 
 #函数creat_combine,参数：一个数组，一个key，产生菜品的组合以字典形式保存在本地
 
@@ -199,7 +143,7 @@ def creat_combine(menu_list,key):
         with open('../data/menu_combine/'+str(key)+'_combine.pickle','wb') as f:
             pickle.dump(menu_dict,f)
     except IOError as err:
-        print(str(err))
+        return(str(err))        #若出错，则将错误信息返回
 
 #函数delete_combine,参数：key，删除该组合
 
@@ -208,7 +152,7 @@ def delete_combine(key):
         file='../data/menu_combine/'+str(key)+'_combine.pickle'
         os.remove(file)
     except:
-        print('文件删除出错！')
+        return('文件删除出错！')       #若出错，则将错误信息返回
 
 #函数get_combine,返回菜品组合,参数：key
 
@@ -218,7 +162,7 @@ def get_combine(key):
             combine=pickle.load(f)
         return(combine)
     except IOError as err:
-        print(str(err))
+        return(str(err))        #若出错，则将错误信息返回
 
 #END
     
